7f5d4ac7b79ab2b90536e70cb704d6d4ba35d69c
shivan-vickers <shivan.vickers@gmail.com>
2019-05-21 11:35:53 -0700

    Add case sensitive search

--- bin/hunt.rb
+++ bin/hunt.rb
@@ -4,16 +4,22 @@ require 'optparse'
 
 files_to_search = Dir.glob('**/*').reject { |e| File.directory? e }
 
+case_sensitive = nil
+
 parser = OptionParser.new
 
 parser.banner = 'Usage: hunt STRING [options]'
 
-parser.on('-t', '--target=PATH', 'Search files or directories matching PATH') do |p|
+parser.on('-i', '--include=PATH', 'Search only files or directories matching PATH') do |p|
   files_to_search.select! { |f| f.include? p }
 end
 
 parser.on('-x', '--exclude=PATH', 'Exclude files or directories matching PATH') do |p|
   files_to_search.reject!{ |f| f.include? p }
+end
+
+parser.on('-c', '--case-sensitive', 'Run a case sensitive search') do
+  case_sensitive= true
 end
 
 parser.on('-h','--help', 'Print this help') do
@@ -28,7 +34,9 @@ end
 
 parser.parse!
 
-target_string = ARGV.shift.downcase
+target_string = ARGV.shift
+
+target_string = target_string.downcase unless case_sensitive
 
 total_matches = 0
 
@@ -39,7 +47,9 @@ files_to_search.each do |filename|
     matches = []
 
     file.each_line do |line|
-      next unless line.downcase.include?(target_string)
+      line = line.downcase unless case_sensitive
+
+      next unless line.include?(target_string)
 
       padding = ' ' * (5 - file.lineno.to_s.length)
       matches << "#{padding}#{file.lineno}| #{line.strip[0..79]}"
